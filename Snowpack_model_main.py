710e0bed25c6262c7dd32c38338673ba951186c3
Ola Thorstensen <167451491+olathorstensen@users.noreply.github.com>
2025-01-09 15:10:16 +0100

    v2.0 Scenario 3
    Model works and seems to recreate the results from the excel model. Here scenario 3 is simulated with 24 h spin-up.

--- Snowpack_model_main.py
+++ Snowpack_model_main.py
@@ -2,17 +2,16 @@
 # v2.0 Neuman boundary conditions 
 #@author: Ola Thorstensen and Thor Parmentier
 # Version update:
-# Ghost cell function works now! Need to use same IC as Simon for Sc.3 sim.
+#   - Implemented neumand boundary condition with the use of 'ghost cell'
+#   - Model v2.0 simulates Scenario 3
+# Comment: 
+#   - Currently not possible to extend runtime to more then 24 h
 
 
 import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-import plotly.graph_objects as go #Whats this?
-import plotly.express as px       #Whats this?
-from plotly.subplots import make_subplots #Whats this?
-
 
 
 ############################    Parameters   ############################ 
@@ -23,7 +22,7 @@ dx = 0.005                # Dist. interval [m]
 pm = 2                   # USE INTEGERS. Plot hour interval
 b_bc = 0                 # Bottom boundary condition, fixed [degC]
 spin_up = 1              # [0] No spin-up, [1] Run spin-up
-sp_runtime = 24        # Spin-up run time [Hours]
+sp_runtime = 24*4        # Spin-up run time [Hours]
 plot_depth = 0.35        # Depth shown in plots measured from surface [m]
 
 
@@ -44,7 +43,7 @@ h = int((3600/dt))       # Number of dt increments between each whole hour [1]
     # Solar properties
 lat = 61                # Latitude [deg]
 sw_con = 1361           # Solar constant [W/m2]
-sw_peak = 100           # Observed shortwave peak [W/m2]
+sw_peak = 110           # Observed shortwave peak [W/m2]
 sw_mod = 659.8259       # Theoretical peak shortwave [W/m2]
 sw_rr = sw_mod/sw_peak  # Reduction ratio
 sw_k = 100               # Solar extinction coefficient (k)
@@ -62,6 +61,7 @@ T1_phase = 6600         # T1 phase shift [s]
 T2_amp = 3              # T2 amplitude [deg C]
 T2_avg = -31            # T2 average temp. [deg C]
 T2_phase = 6600         # T2 phase shift [s]
+
 
 ###########################    Functions    ###########################
 def Solar_rad(h_angle, iy):
@@ -88,8 +88,9 @@ def Solar_extinction(x0,x1,sw):
     x1 = x1/100 # con. cm -> m
     sw_joules = sw*dt*(1-mt.exp(-sw_k*depth)) # Radiation convertet to Joules
     solar_array[5,iy] = sw_joules
+    
     A_sw = sw_joules * sw_k                   # A-term in the SW extinction formula
-    sw_partition = ((A_sw/-sw_k) * (mt.exp(-sw_k*x1) - mt.exp(-sw_k*x0)) /cp*mass)
+    sw_partition = ((A_sw/-sw_k) * (mt.exp(-sw_k*x1) - mt.exp(-sw_k*x0))) /(cp*mass)
     return sw_partition
 
 
@@ -117,26 +118,19 @@ def Heat_flow(ix, iy, grid, neuman):
             t1 = ghost_cell[iy]
             t2 = grid[ix, iy] 
             t3 = grid[ix+1, iy]
-            T = t2 + r*((-2 * t2) + t1 + t3) 
-        
-            
+            T = t2 + r*((-2 * t2) + t1 + t3)       
         else:
             iy = iy-1                                        
             t1 = grid[ix-1, iy]
             t2 = grid[ix, iy] 
             t3 = grid[ix+1, iy]
-            T = t2 + r*((-2 * t2) + t1 + t3)      
-                        
-   
-        
+            T = t2 + r*((-2 * t2) + t1 + t3)          
     else:
         iy = iy-1                                        
         t1 = grid[ix-1, iy]
         t2 = grid[ix, iy] 
         t3 = grid[ix+1, iy]
         T = t2 + r*((-2 * t2) + t1 + t3) 
-   
-     
     return T
 
 
@@ -176,12 +170,12 @@ def Facet_growth(ix, iy):
 nx = int(depth/dx)
 ny = int((runtime * 60 * 60) / dt)
 
-
 temp = np.zeros([nx+1, ny+1], dtype=float)  # Main snowpack grid
 vp = np.zeros([nx+1, ny+1], dtype=float)    # Vapor pressure grid
 vpg = np.zeros([nx+1, ny+1], dtype=float)    # Vapor pressure gradient grid
 fgr = np.zeros([nx+1, ny+1], dtype=float)    # Facet growth rate grid
 fg =  np.zeros([nx+1, ny+1], dtype=float)    # Facet growth grid
+
 
     # Axis and time
 x = np.linspace(0, depth*100, nx+1) # Depth axis [cm]
@@ -199,8 +193,6 @@ ic = np.linspace(-16, 0, nx +1)
 temp[:, 0] = np.round(ic, 4)
 
 
-
-
     # Boundary condition
 # Bottom BC
 temp[-1,:] = b_bc * np.ones(ny+1, dtype=float)
@@ -211,15 +203,7 @@ hour_angle = np.linspace(-180, 180, int(h * 24) + 1)
 solar_array = np.zeros([6,ny+1], dtype=float) # Content: hour angle, elevation, zenith, rad, rad scaled, joules
 T_array = np.zeros([4,ny+1], dtype=float)
 
-
-
-
-
-
-
-
-    
-        
+   
 # Prints of shapes and numbers (all are numbers and none are shapes)
 print('r_number:', r)
 print('a_number:', a)
@@ -283,72 +267,36 @@ if spin_up == 1:
 
 for iy in np.arange(1, ny+1, dtype=int):
     ghost_cell[iy-1] = Sensible_longwave_heat(y[iy], temp[0,iy-1], iy)
-    
-    
-    for ix in np.arange(0, nx, dtype=int):
         
+    for ix in np.arange(0, nx, dtype=int):    
         sw_in = Solar_rad(hour_angle[iy], iy)
-        
         temp[ix, iy] = Heat_flow(ix, iy, temp, 1) + Solar_extinction(x[ix+0],x[ix+1],sw_in) 
         
+   
+for iy in np.arange(0, ny+1, dtype=int):
+    for ix in np.arange(0, nx+1, dtype=int):
+        vp[ix, iy] = Vapor_pressure(ix, iy, temp[ix, iy])
+        
+for iy in np.arange(0, ny+1, dtype=int):
+    for ix in np.arange(0, nx, dtype=int):
+        vpg[ix, iy] = Vapor_pressure_gradient(ix, iy)
+        
+for iy in np.arange(0, ny+1, dtype=int):
+    for ix in np.arange(0, nx, dtype=int):
+        fgr[ix, iy] = Facet_growth_rate(ix, iy)
+        
+for iy in np.arange(0, ny+1, dtype=int):
+    for ix in np.arange(0, nx, dtype=int):
+        fg[ix, iy] = Facet_growth(ix, iy)
+
+net_growth = np.sum(fg, axis = 1)
 
 
-
-# sw_in = 110
-# iy = 1
-# ix = 0
-
-# ghost_cell[iy-1] = Sensible_longwave_heat(y[iy], temp[0,iy-1], iy)
-# #sw_in = Solar_rad(hour_angle[iy], iy)
-        
-# temp[ix, iy] = Heat_flow(ix, iy, temp, 1) + Solar_extinction(x[ix+0],x[ix+1],sw_in)     
-
-# iy = 1
-# ix = 1
-
-# ghost_cell[iy-1] = Sensible_longwave_heat(y[iy], temp[0,iy-1], iy)
-# #sw_in = Solar_rad(hour_angle[iy], iy)
-        
-# temp[ix, iy] = Heat_flow(ix, iy, temp, 1) + Solar_extinction(x[ix+0],x[ix+1],sw_in)     
-
-       
-# iy = 1
-# ix = 2
-
-# ghost_cell[iy-1] = Sensible_longwave_heat(y[iy], temp[0,iy-1], iy)
-# #sw_in = Solar_rad(hour_angle[iy], iy)
-        
-# temp[ix, iy] = Heat_flow(ix, iy, temp, 1) + Solar_extinction(x[ix+0],x[ix+1],sw_in)        
-        
-    
-        
-    
-        
-        
-# for iy in np.arange(0, ny+1, dtype=int):
-#     for ix in np.arange(0, nx+1, dtype=int):
-#         vp[ix, iy] = Vapor_pressure(ix, iy, temp[ix, iy])
-        
-# for iy in np.arange(0, ny+1, dtype=int):
-#     for ix in np.arange(0, nx, dtype=int):
-#         vpg[ix, iy] = Vapor_pressure_gradient(ix, iy)
-        
-# for iy in np.arange(0, ny+1, dtype=int):
-#     for ix in np.arange(0, nx, dtype=int):
-#         fgr[ix, iy] = Facet_growth_rate(ix, iy)
-        
-# for iy in np.arange(0, ny+1, dtype=int):
-#     for ix in np.arange(0, nx, dtype=int):
-#         fg[ix, iy] = Facet_growth(ix, iy)
-
-# net_growth = np.sum(fg, axis = 1)
 
 ###################################################################
 
-
-
 # PLot of results:
-xticks = np.arange(6,-20,-2)
+xticks = np.arange(0,-20,-2)
 pd = int(plot_depth/dx)
 
 for p in np.arange(0, ny+1, h*pm):
@@ -364,50 +312,50 @@ plt.xticks(xticks)
 plt.show()
 
 
-# for p in np.arange(0, ny+1, h*pm):
-#     plt.plot(vp[:pd,p], x[:pd], label= f"Time {y_t[p]}") # Plot every whole hour
+for p in np.arange(0, ny+1, h*pm):
+    plt.plot(vp[:pd,p], x[:pd], label= f"Time {y_t[p]}") # Plot every whole hour
 
-# plt.gca().invert_yaxis()
-# plt.title('Vapor pressure')
-# plt.xlabel('vp [mb] ')
-# plt.ylabel('Depth [cm]')
-# plt.legend()
-# plt.grid(alpha=0.5)
-# plt.show()
+plt.gca().invert_yaxis()
+plt.title('Vapor pressure')
+plt.xlabel('vp [mb] ')
+plt.ylabel('Depth [cm]')
+plt.legend()
+plt.grid(alpha=0.5)
+plt.show()
 
 
-# for p in np.arange(0, ny+1, h*pm):
-#     plt.plot(vpg[:pd,p], x[:pd], label= f"{y_t[p]}") # Plot every whole hour
+for p in np.arange(0, ny+1, h*pm):
+    plt.plot(vpg[:pd,p], x[:pd], label= f"{y_t[p]}") # Plot every whole hour
     
-# plt.gca().invert_yaxis()
-# plt.title('Vapor pressure gradient')
-# plt.xlabel('vpg [mb/m] ')
-# plt.ylabel('Depth [cm]')
-# plt.legend()
-# plt.grid(alpha=0.5)
-# plt.show()
+plt.gca().invert_yaxis()
+plt.title('Vapor pressure gradient')
+plt.xlabel('vpg [mb/m] ')
+plt.ylabel('Depth [cm]')
+plt.legend()
+plt.grid(alpha=0.5)
+plt.show()
 
 
-# for p in np.arange(0, ny+1, h*pm):
-#     plt.plot(fgr[:pd,p], x[:pd], label= f"{y_t[p]}") # Plot every whole hour
+for p in np.arange(0, ny+1, h*pm):
+    plt.plot(fgr[:pd,p], x[:pd], label= f"{y_t[p]}") # Plot every whole hour
     
-# plt.gca().invert_yaxis()
-# plt.title('Facet growth rate')
-# plt.xlabel('Facet growth rate [nm/s] ')
-# plt.ylabel('Depth [cm]')
-# plt.legend()
-# plt.grid(alpha=0.5)
-# plt.show()
+plt.gca().invert_yaxis()
+plt.title('Facet growth rate')
+plt.xlabel('Facet growth rate [nm/s] ')
+plt.ylabel('Depth [cm]')
+plt.legend()
+plt.grid(alpha=0.5)
+plt.show()
 
 
-# plt.plot(net_growth[:pd], x[:pd])
-# plt.gca().invert_yaxis()
-# plt.title('Net facet growth')
-# plt.xlabel('Net growth [mm] ')
-# plt.ylabel('Depth [cm]')
-# #plt.legend()
-# plt.grid(alpha=0.5)
-# plt.show()
+plt.plot(net_growth[:pd], x[:pd])
+plt.gca().invert_yaxis()
+plt.title('Net facet growth')
+plt.xlabel('Net growth [mm] ')
+plt.ylabel('Depth [cm]')
+#plt.legend()
+plt.grid(alpha=0.5)
+plt.show()
 
 
 
